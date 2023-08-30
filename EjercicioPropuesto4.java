429ad08ec0c91d78ec192a02ba18fd6bdde392e8
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 16:23:09 -0500

    Agregados comentarios

--- EjercicioPropuesto4.java
+++ EjercicioPropuesto4.java
@@ -9,11 +9,12 @@ public class EjercicioPropuesto4 {
         Scanner entrada=new Scanner(System.in); 
         int edad_Juan, edad_Alberto, edad_Ana, edad_mama; //Declaracion de variables (edad).
         System.out.println("Ingrese la edad de Juan: ");
-        edad_Juan=entrada.nextInt();
-        edad_Alberto=(2*edad_Juan)/3;
-        edad_Ana=(4*edad_Juan)/3;
-        edad_mama=edad_Juan+edad_Alberto+edad_Ana;
-        System.out.println("La Edad de Juan es: "+edad_Juan);
+        edad_Juan=entrada.nextInt(); //Se calcula edad de Juan
+        edad_Alberto=(2*edad_Juan)/3; //Se calcula edad de Alberto
+        edad_Ana=(4*edad_Juan)/3; //Se calcula edad de Ana
+        edad_mama=edad_Juan+edad_Alberto+edad_Ana; //Se calcula edad de la mamá.
+        //Salidas
+        System.out.println("La edad de Juan es: "+edad_Juan);
         System.out.println("La edad de Alberto es: "+edad_Alberto);
         System.out.println("La edad de Ana es: "+edad_Ana);
         System.out.println("La edad de la mama de Juan es: "+edad_mama);
